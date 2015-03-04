7977a7e5e0e7a2d46450179c1fe9f744b33d8ee7
Dennis Kempin <denniskempin@chromium.org>
2015-03-04 14:35:05 -0800

    Uprev to 0.2

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 import os
 from setuptools import setup, find_packages
 
-version = "0.1"
+version = "0.2"
 url = 'https://github.com/denniskempin/safetynet'
 description = ("Type documentation and checking for python")
 
